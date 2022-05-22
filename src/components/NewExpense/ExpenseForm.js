9cbea02f9dd3b62af03705b841364a7e8dcd7ad8
Farisy1998 <salmanulfarisyk123@gmail.com>
2022-05-22 22:03:30 +0530

    changes commited

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -21,7 +21,7 @@ const ExpenseForm = (props) => {
 
         const expenses = {
             title: title,
-            amount: amount,
+            amount: +amount,
             date: new Date(date)
         };
         props.onSaveExepenseData(expenses);
